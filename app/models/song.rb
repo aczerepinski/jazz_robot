b041fc532d1eed2f074e20b68c9b513f4006117a
aczerepinski <aczerepinski@gmail.com>
2015-05-21 08:53:03 -0400

    add chord display logic

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,19 @@
 class Song < ActiveRecord::Base
+
+
+  def chords_array
+    array = array_from_string
+    group_by_measure(array)
+  end
+
+  private
+
+  def group_by_measure(array)
+    array.each_slice(chords_per_measure).to_a
+  end
+
+  def array_from_string
+    chords.split(",").map{|c|c.strip}
+  end
+
 end
